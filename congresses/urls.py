b06515e81d85ae3abb927d231f3c52c1d17f01a0
Aiven Timptner <aiven@timptner.de>
2023-12-28 21:20:33 +0100

    Update url paths

--- congresses/urls.py
+++ congresses/urls.py
@@ -4,7 +4,9 @@ from congresses import views
 
 app_name = 'congresses'
 urlpatterns = [
-    path('', views.CongressesListView.as_view(), name='congress_list'),
-    path('<int:congress_id>/participants/', views.ParticipantListView.as_view(), name='participant_list'),
-    path('<int:congress_id>/participants/add/', views.ParticipantCreateView.as_view(), name='create_participant'),
+    path('congresses/', views.CongressesListView.as_view(), name='congress_list'),
+    path('congresses/<int:congress_id>/add_participant/',
+         views.ParticipantCreateView.as_view(),
+         name='create_participant'),
+    path('congresses/<int:congress_id>/participants/', views.ParticipantListView.as_view(), name='participant_list'),
 ]
--- fatama/urls.py
+++ fatama/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('congresses.urls')),
     path('accounts/', include('accounts.urls')),
-    path('congresses/', include('congresses.urls')),
     path('admin/', admin.site.urls),
 ]
