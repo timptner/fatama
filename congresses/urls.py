3bbfd7d62b270dcb3f5c4b8b5320752b4d0a1e90
Aiven Timptner <aiven@timptner.de>
2023-12-31 14:28:52 +0100

    Remove portrait create button

--- congresses/urls.py
+++ congresses/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('congresses/', views.CongressListView.as_view(), name='congress-list'),
     path('congresses/<int:pk>/', views.CongressDetailView.as_view(), name='congress-detail'),
     path('congresses/<int:pk>/add_attendance/', views.AttendanceCreateView.as_view(), name='create-attendance'),
-    path('participants/<int:pk>/add_profile/', views.PortraitCreateView.as_view(), name='create-portrait'),
+    path('participants/<int:pk>/add_portrait/', views.PortraitCreateView.as_view(), name='create-portrait'),
     path('attendances/update_seats/', views.SeatFormView.as_view(), name='update-seats'),
 ]
