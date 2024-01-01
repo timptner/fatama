83478fa6afa70b1001a1ae5a5b007c81fd989ea8
Aiven Timptner <aiven@timptner.de>
2024-01-01 17:20:33 +0100

    Add redirect for landing page

--- fatama/urls.py
+++ fatama/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect(reverse('congresses:congress-list'))),
     path('', include('congresses.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
