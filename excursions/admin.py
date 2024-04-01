ae8cfec5c5d36f726b880c7518a88306c096e0c9
Aiven Timptner <aiven@timptner.de>
2024-04-01 18:53:17 +0200

    Update admin views

--- excursions/admin.py
+++ excursions/admin.py
@@ -5,9 +5,11 @@ from excursions.models import Excursion, Order
 
 @admin.register(Excursion)
 class ExcursionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title"]
+    list_filter = ["congress"]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display =["participant", "priority"]
+    list_filter = ["excursion"]
